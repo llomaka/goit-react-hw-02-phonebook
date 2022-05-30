58b05e89a38eaee887648f761a1ff19f71fcefb4
Lilia Lomaka <llomaka80@gmail.com>
2022-05-30 18:51:27 +0300

    Додана валідація форми js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,19 +25,23 @@ export default class App extends Component {
       return alert("Please provide the contact's phone number!");
     if (this.state.contacts.find(contact => contact.name.toLowerCase() === document.querySelector("#contact-name").value.toLowerCase()))
       return alert(`${document.querySelector("#contact-name").value} is already in Contacts List!`);
-    this.setState(prevState => {
-      const newArray = [...prevState.contacts];
-      newArray.push({
-        id: nanoid(),
-        name: document.querySelector("#contact-name").value,
-        number: document.querySelector("#contact-number").value,
+    if (!/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(document.querySelector("#contact-name").value))
+      return alert("Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan");
+    if (!/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/.test(document.querySelector("#contact-number").value))
+      return alert("Phone number must be digits and can contain spaces, dashes, parentheses and can start with +");
+      this.setState(prevState => {
+        const newArray = [...prevState.contacts];
+        newArray.push({
+          id: nanoid(),
+          name: document.querySelector("#contact-name").value,
+          number: document.querySelector("#contact-number").value,
+        });
+        document.querySelectorAll('#contact-form input').forEach(input => input.value = '');
+        return {
+          contacts: newArray,
+          filter: prevState.filter,
+        }
       });
-      document.querySelectorAll('#contact-form input').forEach(input => input.value = '');
-      return {
-        contacts: newArray,
-        filter: prevState.filter,
-      }
-    });
   }
 
   deleteContact = (event) => {
