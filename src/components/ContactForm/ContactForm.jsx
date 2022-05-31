202da8692727dee7a0fbbe7ab126eb69852cf14e
Lilia Lomaka <llomaka80@gmail.com>
2022-05-31 13:14:31 +0300

    Додавання унікальних ідентифікаторів для полів введення

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Button from "components/Button";
+import { nanoid } from 'nanoid';
 import styles from "./ContactForm.module.css";
 
 export default class ContactForm extends Component {
@@ -8,6 +9,9 @@ export default class ContactForm extends Component {
     name: '',
     number: ''
   };
+
+  nameInputId = nanoid();
+  numberInputId = nanoid();
 
   static propTypes = {
     onSubmit: PropTypes.func.isRequired,
@@ -38,7 +42,11 @@ export default class ContactForm extends Component {
         onSubmit={this.handleSubmit}
       >
         <div className={styles.fields}>
-          <label className={styles.label} htmlFor="contact-name">Name *</label>
+          <label
+            className={styles.label}
+            htmlFor={this.nameInputId}>
+            Name *
+          </label>
           <input
             className={styles.input}
             type="text"
@@ -46,14 +54,18 @@ export default class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            id="contact-name"
+            id={this.nameInputId}
             placeholder="John Smith"
             onChange={this.handleInputChange}
             value={this.state.name}
           />
         </div>
         <div className={styles.fields}>
-          <label className={styles.label} htmlFor="contact-number">Number *</label>
+          <label
+            className={styles.label}
+            htmlFor={this.numberInputId}>
+            Number *
+          </label>
           <input
             className={styles.input}
             type="tel"
@@ -61,7 +73,7 @@ export default class ContactForm extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            id="contact-number"
+            id={this.numberInputId}
             placeholder="050-123-23-23"
             onChange={this.handleInputChange}
             value={this.state.number}
